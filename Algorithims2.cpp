c0a833bff0d3c4e9dbd02277d5c63144b1938b02
Davevis93 <davevis93@gmail.com>
2017-07-07 09:10:35 -0500

    Update Algorithims2.cpp

--- Algorithims2.cpp
+++ Algorithims2.cpp
@@ -52,14 +52,27 @@ int main(){
 	return 0;
 }
 
+void initializeVector(vector<int>& list) {
+	int vectorInput;
+	
+	cout << "Enter a number or 0 to exit: ";
+	cin >> vectorInput;
+
+	while (vectorInput != 0) {		
+		list.push_back(vectorInput);
+		cout << "Enter a number or 0 to exit: ";
+		cin >> vectorInput;
+	}
+}
+
 //----------------------SWAP FUNCTIONS------------------------------------
-//void swap(vector<int>& list, int p, int q){
-//	if (p != q){
-//		int first = list[p];
-//		list[p] = list[q];
-//		list[q] = first;
-//	}
-//}
+void swap(vector<int>& list, int p, int q){
+	if (p != q){
+		int first = list[p];
+		list[p] = list[q];
+		list[q] = first;
+	}
+}
 
 void swapArray(int array[], int p, int q){
 	if (p != q){
@@ -70,34 +83,34 @@ void swapArray(int array[], int p, int q){
 }
 
 //---------------------MIN INDEX FUNCTIONS--------------------------------
-//int minIndex(vector<int>& list){
-//
-//	if (list.size() == 0){
-//		throw logic_error("Error! List is empty!");
-//	}
-//
-//	int index = 0;
-//	for (int i = 0; i < list.size(); i++){
-//		if (list[i] < list[index]){ //index is holder of min value index position. If a new position is lower than old min than min updates
-//			index = i;
-//		}
-//	}
-//	return index; //Returns min index position. doing return list[index] would return min value
-//}
-//
-//int minIndex4SelectionSort(vector<int>& list, int start){
-//	if (list.size() == 0){
-//		throw logic_error("Error! List is Empty!");
-//	}
-//
-//	int index = start;//
-//	for (int i = start; i < list.size(); i++){
-//		if (list[i] < list[index]){ //index is holder of min value index position. If a new position is lower than old min than min updates
-//			index = i;
-//		}
-//	}
-//	return index; //Returns min index position. doing return list[index] would return min value
-//}
+int minIndex(vector<int>& list){
+
+	if (list.size() == 0){
+		throw logic_error("Error! List is empty!");
+	}
+
+	int index = 0;
+	for (int i = 0; i < list.size(); i++){
+		if (list[i] < list[index]){ //index is holder of min value index position. If a new position is lower than old min than min updates
+			index = i;
+		}
+	}
+	return index; //Returns min index position. doing return list[index] would return min value
+}
+
+int minIndex4SelectionSort(vector<int>& list, int start){
+	if (list.size() == 0){
+		throw logic_error("Error! List is Empty!");
+	}
+
+	int index = start;//
+	for (int i = start; i < list.size(); i++){
+		if (list[i] < list[index]){ //index is holder of min value index position. If a new position is lower than old min than min updates
+			index = i;
+		}
+	}
+	return index; //Returns min index position. doing return list[index] would return min value
+}
 
 int minIndexArray(int array[], int size, int start){
 	if (size <= 0){
@@ -116,20 +129,20 @@ int minIndexArray(int array[], int size, int start){
 }
 
 //--------------------BUBBLE SORT FUNCTIONS-------------------------------
-//void bubbleSort(vector<int>& list){
-//	bool didSwap = true;
-//
-//	while (didSwap){ //If it ever reads that a swap was done then do another full pass through the numbers
-//		didSwap = false; //Keep running until you don't do a swap
-//
-//		for (int i = 1; i < list.size(); i++){
-//			if (list[i] < list[i - 1]){ //If i is less than previous i then swap. If i is NOT less than previous i then the if loop does nothing
-//				swap(list, i, i-1); //Just call the swap function made earlier. p is i, q is i-1
-//				didSwap = true; //Set to true to show we did a swap and reverse the previous false setting
-//			}
-//		}
-//	}
-//}
+void bubbleSort(vector<int>& list){
+	bool didSwap = true;
+
+	while (didSwap){ //If it ever reads that a swap was done then do another full pass through the numbers
+		didSwap = false; //Keep running until you don't do a swap
+
+		for (int i = 1; i < list.size(); i++){
+			if (list[i] < list[i - 1]){ //If i is less than previous i then swap. If i is NOT less than previous i then the if loop does nothing
+				swap(list, i, i-1); //Just call the swap function made earlier. p is i, q is i-1
+				didSwap = true; //Set to true to show we did a swap and reverse the previous false setting
+			}
+		}
+	}
+}
 
 void bubbleSortArray(int array[], int size){
 	bool didSwapArray = true;
@@ -146,9 +159,14 @@ void bubbleSortArray(int array[], int size){
 
 
 //--------------------SELECTION SORT FUNCTIONS----------------------------
-//void selectionSort(vector<int>& list){
-//
-//}
+void selectionSort(vector<int>& list) {
+	if (list.size() > 1){
+		for (int ScanStart = 0; ScanStart < list.size(); ScanStart++) {
+			int minIndex = searchMinIndex(list, ScanStart);
+			swap(list, ScanStart, minIndex);
+		}
+	}
+}
 
 void selectionSortArray(int array[], const int size){
 	for (int i = 0; i < size; i++){
